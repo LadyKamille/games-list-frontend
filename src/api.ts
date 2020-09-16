708e5f7487b0f07659fd1b0b70ad6fc58bc0aa63
LadyKamille <kamionorato@gmail.com>
2020-09-16 15:52:52 -0400

    Setup axios instance

---
+++ src/api.ts
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+const xsrfCookieName: string | undefined = document.cookie
+  .split(";")
+  ?.find(cookie => cookie.startsWith('csrftoken'))
+  ?.split('=')[1];
+
+export default axios.create({
+  baseURL: `http://localhost.com:8000/`,
+  xsrfHeaderName: 'X-XSRF-TOKEN',
+  xsrfCookieName,
+});
